3f27c0bc54fc1cef9a591e82f43ee019935b1123
E1maa <118517467+E1maa@users.noreply.github.com>
2022-12-09 18:13:13 +0100

    rename files

---
+++ client/src/main/java/at/jku/ImportController.java
@@ -0,0 +1,24 @@
+package at.jku;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class ImportController {
+
+
+    @FXML
+    private void onActionRooms() throws IOException {
+        DigitalTwinApp.setRoot("allrooms");
+    }
+
+    @FXML
+    private void onActionHome() throws IOException {
+        DigitalTwinApp.setRoot("primary");
+    }
+
+    @FXML
+    private void onActionNewRoom() throws IOException {
+        DigitalTwinApp.setRoot("newroom");
+    }
+}
